39f2f95802fca05842f373eb6fd17399e2aaa481
Antonio Cordero Serrano <152612563+AntonioCorderoSerrano@users.noreply.github.com>
2025-03-04 20:40:43 +0100

    Añadido compresion para moviles

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    compress: true, 
+  };
+  
+  export default nextConfig;
+  
